2f3950ec5dcfc37592bfb4e7a976a87af7e51a07
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-23 03:49:19 +0200

    feat(Header): add content to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,10 @@
 const Header = () => {
-    return (
-        <header className="App-header">
-            <p>
-                Hello, World
-            </p>
-        </header>
-    );
+    const handleNameChange = () => {
+        const names = ["Timi", "Andreea", "Andrei", "Kuku"];
+        const int = Math.floor(Math.random() * 4);
+        return names[int];
+    };
+    return <header className="App-header">Hello, {handleNameChange()}</header>;
 };
 
 export default Header;
