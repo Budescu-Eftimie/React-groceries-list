ae51aeb852b3f57865e3a16f7b283173e7d71c7b
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-23 04:02:53 +0200

    feat(Footer): add logic to Footer
    This commit adds the logic to Footer and displays copyright and date.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "../Header/Header";
 import Content from "../Content/Content";
+import Footer from "../Footer/Footer";
 
 function App() {
     return (
         <div className="App">
             <Header />
             <Content />
+            <Footer />
         </div>
     );
 }
