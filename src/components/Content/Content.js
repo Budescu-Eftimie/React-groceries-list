57baaf9667786baff5aaa973e5bebc64d535182d
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-24 05:06:19 +0200

    feat(Content): add return list to dom

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { FaTrashAlt } from "react-icons/fa";
 
 const Content = () => {
     const [items, setItems] = useState([
@@ -19,7 +20,19 @@ const Content = () => {
         },
     ]);
 
-    return <main></main>;
+    return (
+        <main>
+            <ul>
+                {items.map((item) => (
+                    <li className="item" key={item.id}>
+                        <input type="checkbox" checked={item.checked} />
+                        <label>{item.item}</label>
+                        <FaTrashAlt role="button" tabIndex="0" />
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
 };
 
 export default Content;
