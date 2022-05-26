847ddad5f0d77c61c68d74147f8b8f4d53c15129
Budescu Eftimie <budescueftimie@gmail.com>
2022-05-26 05:09:03 +0200

    feat(Content.js): add handleCheck function

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -20,12 +20,24 @@ const Content = () => {
         },
     ]);
 
+    const handleCheck = (id) => {
+        const listItems = items.map((item) =>
+            item.id === id ? { ...item, checked: !item.checked } : item
+        );
+        setItems(listItems);
+        localStorage.setItem("shopingList", JSON.stringify(listItems));
+    };
+
     return (
         <main>
             <ul>
                 {items.map((item) => (
                     <li className="item" key={item.id}>
-                        <input type="checkbox" checked={item.checked} />
+                        <input
+                            type="checkbox"
+                            checked={item.checked}
+                            onChange={() => handleCheck(item.id)}
+                        />
                         <label>{item.item}</label>
                         <FaTrashAlt role="button" tabIndex="0" />
                     </li>
